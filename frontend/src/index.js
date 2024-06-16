104ac73c9cd7cc3d946a0a004cdc1ccf69f48b1c
dev_dimeji✔ <123726979+lukmanuthmanabubakr@users.noreply.github.com>
2024-06-16 11:43:18 +0100

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,5 +10,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: 
+// If you want to start measuring performance in your app, pass a function 
