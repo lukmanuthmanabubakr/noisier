a2088478baf2591ade515712de9b919219284f04
dev_dimeji✔ <123726979+lukmanuthmanabubakr@users.noreply.github.com>
2024-06-16 11:43:05 +0100

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -11,4 +11,4 @@ root.render(
 );
 
 // If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
+// to log results (for example: 
